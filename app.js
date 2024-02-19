f46c1d4c9bb87a7339bceffa13c338fd557c0773
cwtshh <gucosta1719@gmail.com>
2024-02-19 20:48:29 -0300

    some fixes

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // cors
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONT_URL,
+    }
+));
 
 // db
 require("./config/db");
